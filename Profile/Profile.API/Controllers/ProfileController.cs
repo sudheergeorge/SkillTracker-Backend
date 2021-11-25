0e39ace01f0c8f33d2b19e77b59adc0475a4a012
Sudheer George <sudheergeorge@gmail.com>
2021-11-25 13:46:14 +0530

    Updated

--- Profile/Profile.API/Controllers/ProfileController.cs
+++ Profile/Profile.API/Controllers/ProfileController.cs
@@ -1,6 +1,7 @@
 ﻿using MediatR;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
 using Profile.Application.Features.Commands.AddProfile;
 using Profile.Application.Features.Commands.UpdateProfile;
 using System.Net;
@@ -13,16 +14,19 @@ namespace Profile.API.Controllers
     public class ProfileController : ControllerBase
     {
         private readonly IMediator _mediator;
+        private readonly ILogger<ProfileController> _logger;
 
-        public ProfileController(IMediator mediator)
+        public ProfileController(IMediator mediator, ILogger<ProfileController> logger)
         {
             _mediator = mediator;
+            _logger = logger;
         }
 
         [HttpGet(Name = "Health")]
         [ProducesResponseType((int)HttpStatusCode.OK)]
         public async Task<ActionResult<int>> Health()
         {
+            _logger.LogInformation("Profile Api- health check.");
             var response = "I am Good!!";
             return Ok(response);
         }
