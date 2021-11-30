cd71798fca52aafbb6725acde1dd8f48d96e5a3e
Sudheer George <sudheergeorge@gmail.com>
2021-11-30 11:21:08 +0530

    Cache related updates

--- Admin/Admin.API/EventBusConsumers/AddProfileConsumer.cs
+++ Admin/Admin.API/EventBusConsumers/AddProfileConsumer.cs
@@ -22,12 +22,10 @@ namespace Admin.API.EventBusConsumers
             _logger = logger;
         }
 
-#pragma warning disable CS1998 // This async method lacks 'await' operators and will run synchronously. Consider using the 'await' operator to await non-blocking API calls, or 'await Task.Run(...)' to do CPU-bound work on a background thread.
         public async Task Consume(ConsumeContext<AddProfileEvent> context)
-#pragma warning restore CS1998 // This async method lacks 'await' operators and will run synchronously. Consider using the 'await' operator to await non-blocking API calls, or 'await Task.Run(...)' to do CPU-bound work on a background thread.
         {
             var command = JsonConvert.DeserializeObject<CacheProfileCommand>(context.Message.Data);
-            _ = _mediator.Send(command);
+            await _mediator.Send(command);
 
             //var escommand = JsonConvert.DeserializeObject<ESCacheProfileCommand>(context.Message.Data);
             //_ = _mediator.Send(escommand);
